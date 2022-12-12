4f601007a2d8ce1dd5733b7e9f12e471a6e90d7e
monther123c <monthertamimi123@gmail.com>
2022-12-12 08:57:27 +0000

    docker

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project: Games API
+ ## auther : monther
+
+python manage.py ~ runserver ~ 
+
+then - 
+ 
+Use http://127.0.0.1:<PORT>/api/v1/games/ to get Games API
---
+++ games/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Game
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import GameSerializer
+
+# Create your views here.
+
+class GameListView(ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class GameDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
